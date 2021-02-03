1df71cebd2700c1ac16e81fe825d5c0946bce4ae
Joaquim Ventura <nowjoka28@gmail.com>
2021-02-02 22:06:52 -0300

    iniciando-back-end-nodejs

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 /**
  *
  */
@@ -17,13 +24,48 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    delete user.password;
+    //delete user.password;
 
-    return response.json(user);
+    const userWithoutPassoword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassoword);
 
   } catch (err) {
     return response.status(400).json({ error: err.message })
   }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated,
+upload.single('avatar'),
+async(request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      const userWithoutPassoword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      return response.json(userWithoutPassoword);
+    } catch (err) {
+      return response.status(400).json({ error: err.message })
+    }
 });
 
 export default usersRouter;
